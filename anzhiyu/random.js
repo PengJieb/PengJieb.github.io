6707a8e632f1860c68711b64843a5edeb961bce2
PengJie <pengjieb@mail.ustc.edu.cn>
2025-02-06 18:28:42 +0800

    Site updated: 2025-02-06 18:28:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/06/omg-dataset/","1970/01/01/hello-world/"];function toRandomPost(){
+var posts=["2025/02/06/omg-dataset/","2025/02/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
